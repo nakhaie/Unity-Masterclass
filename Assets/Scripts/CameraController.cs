3a05ebf9432fa99a42c732672c76a0a08c9dabc1
amin.nakhaie <amin.nakhaie@gmail.com>
2022-08-02 17:44:47 +0430

    First Push Project body + player camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    
+    public float CameraAngleLimitMin;
+    public float CameraAngleLimitMax;
+    
+    public Transform playerBody;
+    private float xRotation = 0;
+
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, CameraAngleLimitMin, CameraAngleLimitMax);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0.0f,0.0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 12;
+    public float gravity = -9.81f;
+
+    public Transform GroundCheck;
+    public float GroundDistance;
+    public LayerMask GroundMask;
+
+    public float JumpHeight = 3f;
+    
+    private CharacterController _controller;
+
+    private Vector3 velocity;
+    private bool isGround;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGround = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+        if (isGround && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        _controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGround)
+        {
+            velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+        
+        _controller.Move(velocity * speed * Time.deltaTime);
+    }
+
+}
